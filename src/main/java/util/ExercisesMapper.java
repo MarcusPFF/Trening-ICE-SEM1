9afb573a4672eb329b26b45d131e95d51576de62
MarcusPFF <marcus.pff03@gmail.com>
2024-12-10 19:48:45 +0100

    initialised dbconnect object in AccountMapper and ExercisesMapper

--- src/main/java/util/ExercisesMapper.java
+++ src/main/java/util/ExercisesMapper.java
@@ -7,11 +7,13 @@ import app.Workout;
 import java.util.List;
 
 public class ExercisesMapper {
+    DBConnector dbconnector;
     Exercises ex;
     Workout wo;
 
 
     public ExercisesMapper() {
+        dbconnector = new DBConnector();
     }
 
     public List<Exercises> loadExercisesData() {
